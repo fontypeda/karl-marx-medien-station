0ea3adb1d17473666ae2dd0740a64d48b0a78f5b
Niklas Alt <altniklas@uni-trier.de>
2018-06-15 11:28:35 +0200

    Added source title (now separated)

--- data/write-sources.py
+++ data/write-sources.py
@@ -35,6 +35,7 @@ for source_idx, source in enumerate(root):
     curr_source = {}
     curr_source["image"] = source.find("image").text
     curr_source["sourceInfo"] = source.find("source-info").text
+    curr_source["sourceTitle"] = source.find("source-title").text
     curr_source["transcription"] = split_paras(source.find("transcription").text)
     sources.append(curr_source)
 
