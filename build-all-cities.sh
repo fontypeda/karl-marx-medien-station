d7ba641c892bf587c519bec79bf1c2cfc1d2484b
Niklas Alt <altniklas@uni-trier.de>
2018-06-16 12:11:33 +0200

    enable zip and localhost options

--- build-all-cities.sh
+++ build-all-cities.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+if [ ! -z $1 ]; then
+  OPTIONS=$1;
+fi
+
 cities="trier bonnberlin koeln paris manchester bruessel london"
 
 for city in $cities; do
-  bash build-city.sh $city
+  bash build-city.sh -c $city $OPTIONS
 done
--- build-city.sh
+++ build-city.sh
@@ -1,41 +1,92 @@
 #!/bin/bash
 
 ## Automatic compilation of media stations
+BUILD_LOCAL_HOST=0
+BUILD_ZIP_FILE=0
 
-if [ -z $1 ]; then
-  echo "You have to pass at least one argument!"
+TMP_DIR="/tmp/km-medien/"
+
+while getopts ":lzc:" opt; do
+  case $opt in
+    l)
+      BUILD_LOCAL_HOST=1
+      ;;
+    z)
+      BUILD_ZIP_FILE=1
+      ;;
+    c)
+      city=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+if [ -z $city ]; then
+  echo "Error: The -c option must be set"
+  echo "Usage: "
+  echo "bash build-city -c london"
+  echo "Exiting..."
   exit 1
 fi
 
-while [ ! -z $1 ]; do
-  city=$1
-  figlet "Building $city"
+# if [ -z $1 ]; then
+#   echo "You have to pass at least one argument!"
+#   exit 1
+# fi
 
-  figlet "Preparing data..."
-  # Copy the style variable
-  cp data/common-vars-$city.scss src/common-vars.scss
-  # Overwrite the house value
-  city_color=$(grep "primary-color" src/common-vars.scss | head -n 1 | cut -d"#" -f2 | cut -d";" -f1)
-  echo $city_color
-  python3 utils/rewrite-house.py "$city_color"
+echo $BUILD_LOCAL_HOST
+echo $BUILD_ZIP_FILE
 
-  # read and write the data
-  cd data/
-  python3 write-biographies.py
-  python3 write-biography-selection.py $city
-  python3 write-letters.py $city
-  python3 write-city-portrait.py $city
-  python3 write-sources.py $city
+if [ ! -d zipfiles ]; then
+  mkdir zipfiles
+fi
 
-  figlet "Copying images"
-  rm -rf ../src/assets/archive/*
-  cp "archive/cities/"$city"/transformed/"*jpg ../src/assets/archive/
+
+figlet "Building $city"
+
+figlet "Preparing data..."
+# Copy the style variable
+cp data/common-vars-$city.scss src/common-vars.scss
+# Overwrite the house value
+city_color=$(grep "primary-color" src/common-vars.scss | head -n 1 | cut -d"#" -f2 | cut -d";" -f1)
+echo $city_color
+python3 utils/rewrite-house.py "$city_color"
+
+# read and write the data
+cd data/
+python3 write-biographies.py
+python3 write-biography-selection.py $city
+python3 write-letters.py $city
+python3 write-city-portrait.py $city
+python3 write-sources.py $city
+
+figlet "Copying images"
+rm -rf ../src/assets/archive/*
+cp "archive/cities/"$city"/transformed/"*jpg ../src/assets/archive/
+
+cd ../
+
+# Compile to localhost for testing (if -l option set)
+if [ $BUILD_LOCAL_HOST -eq 1 ]; then
   figlet "Compiling to localhost..."
-  # Compile to localhost for testing
-  cd ../
   ng build --prod \
     --output-path="/var/www/html/km-medien/"$city \
     --base-href="/km-medien/"$city"/"
-  shift 1
+fi
 
-done
+# zip file (if -z option set)
+if [ $BUILD_ZIP_FILE -eq 1 ]; then
+  figlet "Compiling for zip file..."
+  if [ ! -d $TMP_DIR ]; then
+    mkdir $TMP_DIR
+  fi
+  ng build --prod \
+    --output-path=$TMP_DIR"/"$city
+  cp autorun.brs $TMP_DIR"/"$city"/"
+  cd $TMP_DIR"/"$city
+  zip -r "../"$city".zip" .
+  cd -
+  cp $TMP_DIR"/"$city".zip" zipfiles/
+fi
